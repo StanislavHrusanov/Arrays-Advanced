fe8cfbf88e85cb7534dc7a985eba4e8753d1bc1e
StanislavHrusanov <stanislavhrusanov@gmail.com>
2022-05-01 17:06:40 +0300

    Add problem solution

---
+++ BuildAWall1.js
@@ -0,0 +1,19 @@
+function buildAWall(heightOfSectionsAsArr) {
+    heightOfSectionsAsArr = heightOfSectionsAsArr.map(Number);
+    let concreteUsedForEverySingleDayAsArr = [];
+    heightOfSectionsAsArr = heightOfSectionsAsArr.filter((x) => x != 30);
+
+    while (heightOfSectionsAsArr.length > 0) {
+
+        concreteUsedForEverySingleDayAsArr.push(heightOfSectionsAsArr.length * 195);
+
+        heightOfSectionsAsArr = heightOfSectionsAsArr.map((x) => x + 1);
+
+        heightOfSectionsAsArr = heightOfSectionsAsArr.filter((x) => x != 30);
+    }
+    let totalConcrete = concreteUsedForEverySingleDayAsArr.reduce((a, b) => a + b);
+    let totalConcreteInPesos = totalConcrete * 1900;
+
+    console.log(`${concreteUsedForEverySingleDayAsArr.join(', ')}\n${totalConcreteInPesos} pesos`);
+}
+buildAWall(['17', '22', '17', '19', '17']);
